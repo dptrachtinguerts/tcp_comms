853d70739ad740de8cfba707604e7a1aa9b15e7b
Humberto <humberto_sasaki@usp.br>
2024-11-14 18:24:46 -0300

    Ajustes de TCP

--- sender.py
+++ sender.py
@@ -2,7 +2,7 @@ import socket
 import struct
 
 # Server information
-SERVER_IP = '10.1.1.125'  # IP of the server
+SERVER_IP = '10.1.1.140'  # IP of the server
 PORT = 12345             # Port to connect to
 
 # Create a socket and connect to the server
@@ -10,13 +10,13 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((SERVER_IP, PORT))
 
 # The integer to send
-integer_to_send = 1234567890123456789  # Example large integer
+integer_to_send = 35456  # Example large integer
 
 # Pack the integer into 8 bytes (unsigned long long format)
 packed_data = struct.pack('!Q', integer_to_send)  # '!Q' means big-endian, 8-byte unsigned integer
 
 # Send the packed data
-client_socket.send(packed_data)
+client_socket.sendall(packed_data)
 
 # Close the connection
 client_socket.close()
--- tcp_sender_CORRETO.py
+++ tcp_sender_CORRETO.py
@@ -14,8 +14,10 @@ use_screen_grab = True
 def get_screen():
     screenshot = ImageGrab.grab(all_screens=True)
     screen = np.array(screenshot)
-    # frame = screen[2160:, 1927:, ::-1]
-    frame = screen[:, :1920, ::-1]
+    frame = screen[:2160, :3840, ::-1]
+    # frame = screen[2160:, 1927:, ::-1] # tela da direita
+    #frame = screen[2160:, 6:1926, ::-1] # tela da esquerda
+    # frame = screen[:, :1920, ::-1]
     print(f"[{time.time()}] ATENÇÃO: esse valor deve ser de 1920x1080 -> valor real: ", frame.shape)
     return frame
 
@@ -47,7 +49,7 @@ obstacles_signature, radar_img = radarext.radar_from_tpn_recording(frame)
 ### Network ###
 packet_size = 1024
 
-client_port = 20000
+client_port = 12345
 client_ip = "10.1.1.140"
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -69,22 +71,25 @@ while(True):
         server_socket.close()
         break
 
-    obstacles_signature, radar_img = radarext.radar_from_tpn_recording(frame)
+    # obstacles_signature, radar_img = radarext.radar_from_tpn_recording(frame)
+    obstacles_signature = frame
 
     cv2.imwrite(file_name, obstacles_signature)
 
     size = os.stat(file_name).st_size
-    server_socket.send(struct.pack('!Q', int(size)))
+    print(size)
+    # packed_data = struct.pack('!Q', int(size))
+    server_socket.sendall(str(size).ljust(8).encode())
 
-    # bytes_sent = 0
-    # with open(file_name, 'rb') as img_file:
-    #     data = img_file.read(packet_size if ((size - bytes_sent)//packet_size != 0) else (size - bytes_sent))
+    bytes_sent = 0
+    with open(file_name, 'rb') as img_file:
+        data = img_file.read(packet_size if ((size - bytes_sent)//packet_size != 0) else (size - bytes_sent))
         
-    #     while bytes_sent < size:
-    #         bytes_sent += server_socket.send(data)
-    #         data = img_file.read(packet_size if ((size - bytes_sent)//packet_size != 0) else (size - bytes_sent))
+        while bytes_sent < size:
+            bytes_sent += server_socket.send(data)
+            data = img_file.read(packet_size if ((size - bytes_sent)//packet_size != 0) else (size - bytes_sent))
 
-    #     img_file.close()
+        img_file.close()
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
@@ -95,4 +100,4 @@ while(True):
         server_socket.close()
         break
 
-    time.sleep(3)
+    # time.sleep(3)
